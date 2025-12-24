047d29e08ec2fd0cfbaab0540343a06548884fe9
momapu <you@example.com>
2025-12-24 21:23:32 +0300

    2st commit

--- index.js
+++ index.js
@@ -14,3 +14,23 @@ console.table({a: 1, b: 2});
 console.log(userName, userAge);
 
 
+const str = 'текст';
+const num = 42;
+const bool = true;
+const empty = null;
+const notDefined = undefined;
+const obj = { key: 'value' };
+
+
+const city = {
+    name: 'My city',
+    population: 8000000
+};
+
+city.name = 'Dzerzhinsk';
+city.country = 'RUSSIA';
+delete city.population;
+
+console.log(city);
+
+
