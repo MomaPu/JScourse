67cde77794ee66d0291c2f6ddc60ace8ed77fafb
momapu <you@example.com>
2025-12-24 21:21:18 +0300

    1st commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const user = {
+    name: 'Андрей',
+    age: 30,
+    isStudent: true
+};
+
+const userName = 'Андрей';
+let userAge = 25;
+userAge = 26;
+
+console.log('Hello world');
+console.dir(console);
+console.table({a: 1, b: 2});
+console.log(userName, userAge);
+
+
