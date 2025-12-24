35e88d8bb37bbfdd7e881191ea9fc9de51ed8ab1
momapu <you@example.com>
2025-12-24 21:41:53 +0300

    10st commit

--- index.js
+++ index.js
@@ -142,3 +142,14 @@ function sum(a, b) { return a + b; }
 
 const multiply = function(a, b) { return a * b; };
 
+const add = (a, b) => a + b;
+console.log(add(2, 3)); // 5
+
+setTimeout(() => console.log("Delayed message"), 1000);
+
+function multiply(value, multiplier = 1) {
+    return value * multiplier;
+}
+console.log(multiply(5)); // 5
+
+
