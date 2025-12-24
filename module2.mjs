08073fce89a676e8075d5fee54f90ebd3fb65b2a
momapu <you@example.com>
2025-12-24 22:10:32 +0300

    22st commit

---
+++ module2.mjs
@@ -0,0 +1,8 @@
+import { sum as add, multiply as mult } from './module.mjs';
+console.log(add(10, 2));
+console.log(mult(10, 2));
+
+
+import greet from './module3.mjs';
+console.log(greet('Andrey'));
+
