69d632f7ef42617190044323edae7914b9509375
momapu <you@example.com>
2025-12-24 21:55:14 +0300

    17st commit

--- index.js
+++ index.js
@@ -219,6 +219,18 @@ if (age < 12) {
 }
 
 
+function sumPositiveNums(a, b) {
+    if (typeof a !== "number" || typeof b !== "number") {
+        return "One of the arguments is not a number";
+    }
+    if (a <= 0 || b <= 0) {
+        return "Numbers are not positive";
+    }
+    return a + b;
+}
+console.log(sumPositiveNums(3, 8));
+
+
 
 
 
